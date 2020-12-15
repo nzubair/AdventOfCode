1a615ac37c8816eaebf3b08bbaa58dfff2ac3821
Nasir Zubair <nzubair@users.noreply.github.com>
2020-12-14 23:19:28 -0500

    Moved input out of the scripts and into separate files for cleaner code.

--- misc/Generate-PuzzleTemplate.ps1
+++ misc/Generate-PuzzleTemplate.ps1
@@ -3,36 +3,59 @@ param(
     [Parameter(Mandatory=$true)]
     [int]$Year,
     [Parameter(Mandatory=$true)]
-    [int]$Day,
-    [Parameter(Mandatory=$true)]
-    [int]$Puzzle
+    [int]$Day
 )
 
-    
+$paths = @()
+
 $Root = Resolve-Path $PSScriptRoot\.. | Select-Object -ExpandProperty Path
 
-$FilePath = Join-Path $Root ("{0}\Day{1:00}-Puzzle-{2}.ps1" -f $Year,$Day,$Puzzle)
+$InputFilePath = Join-Path $Root ("{0}\Day{1:00}-input.txt" -f $Year,$Day)
 
 $templateContent = @"
 <#
 
 Advent of Code - http://adventofcode.com
-$($Year) - Day $($Day) - Puzzle $($Puzzle)
+{YEAR} - Day {DAY} - Puzzle {PUZZLE}
 
 <DESCRIPTION>
 
 Author: Nasir Zubair
 
 #>
-
 #region input
-`$input = `"`";
+`$Day = `"{DAY2}`"
+`$InputFile = Join-Path `$PSScriptRoot `"Day`$(`$Day)-input.txt`"
 
-`$input = @`"
-
-`"@ -split `"\n|\r\n`"
+if(-Not (Test-Path `$InputFile)) 
+{
+    Write-Warning `"Input file `$(`$InputFile) not found.`"
+    exit 1
+}
+`$input = Get-Content -Path `$InputFile
 #endregion input
 
 "@
 
-$templateContent | Set-Content -Path $FilePath -Encoding UTF8
+"" | Set-Content -Path $InputFilePath -Encoding UTF8
+$paths += $InputFilePath
+
+1..2 | ForEach-Object { 
+
+    $PuzzlePath = Join-Path $Root ("{0}\Day{1:00}-Puzzle-{2}.ps1" -f $Year,$Day,$_)
+
+    $content = $templateContent -replace "{YEAR}",$Year
+    $content = $content -replace "{DAY}",$Day
+    $content = $content -replace "{DAY2}",($Day).ToString("D2")
+    $content = $content -replace "{PUZZLE}",$_
+
+    $content | Set-Content -Path $PuzzlePath -Encoding UTF8
+    $paths += $PuzzlePath
+}
+
+
+if($Host.Name -eq "Visual Studio Code Host")
+{
+    $paths | ForEach-Object { code -r $_}
+}
+ 
