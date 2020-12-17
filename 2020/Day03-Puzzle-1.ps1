3b29dd56c549d98010cdcbd725ad89bb927403e3
Nasir Zubair <nzubair@users.noreply.github.com>
2020-12-16 23:33:20 -0500

    Add solutions for Day 3

---
+++ 2020/Day03-Puzzle-1.ps1
@@ -0,0 +1,55 @@
+﻿<#
+
+Advent of Code - http://adventofcode.com
+2020 - Day 3 - Puzzle 1
+
+The puzzle says that pattern repeats to the right, we don't actually need to extend 
+the input by duplicating it. Instead, we can wrap around by calculating modulo (remainder)
+of the current position and width. 
+
+Author: Nasir Zubair
+
+#>
+#region input 
+$Day = "03"
+$InputFile = Join-Path $PSScriptRoot "Day$($Day)-input.txt"
+
+if(-Not (Test-Path $InputFile)) 
+{
+    Write-Warning "Input file $($InputFile) not found."
+    exit 1
+}
+$input = Get-Content -Path $InputFile
+#endregion input
+
+$width = ($input[0].Length)
+$end = $input.Length - 1
+
+$slope = [pscustomobject]@{"right" = 3; "down" = 1 }
+
+$trees = 0;
+
+# starting point
+$x = $y = 0
+
+do
+{
+    # take a step
+    $x += $slope.right
+    $y += $slope.down
+
+    # check for tree
+    $right = $x % $width
+    $down = $y
+
+    if(($input[$down][$right]) -eq "#") 
+    {
+         $trees++ 
+    }
+
+    $input[$down] = $news.ToString();
+
+    "right: $right  down: $down  ($x,$y)  :: $($input[$down][$right])"
+} while($y -lt $end)
+
+"Trees: $trees"
---
+++ 2020/Day03-Puzzle-2.ps1
@@ -0,0 +1,74 @@
+﻿<#
+
+Advent of Code - http://adventofcode.com
+2020 - Day 3 - Puzzle 2
+
+Same approach as the Puzzle 1. However, now we have to calculate tree 
+for multiple slopes. so an outer loop is introduced to loops through
+and array of sloped. 
+
+Author: Nasir Zubair
+
+#>
+#region input
+$Day = "03"
+$InputFile = Join-Path $PSScriptRoot "Day$($Day)-input.txt"
+
+if(-Not (Test-Path $InputFile)) 
+{
+    Write-Warning "Input file $($InputFile) not found."
+    exit 1
+}
+$input = Get-Content -Path $InputFile
+#endregion input
+
+$width = ($input[0].Length)
+$end = $input.Length - 1
+
+$alltrees = @()
+
+$slopes = @(
+    [pscustomobject]@{"right" = 1; "down" = 1},
+    [pscustomobject]@{"right" = 3; "down" = 1},
+    [pscustomobject]@{"right" = 5; "down" = 1},
+    [pscustomobject]@{"right" = 7; "down" = 1},
+    [pscustomobject]@{"right" = 1; "down" = 2}
+)
+
+
+foreach($slope in $slopes)
+{
+    $trees = 0;
+
+    # starting point
+    $x = $y = 0
+
+    do
+    {
+        # take a step
+        $x += $slope.right
+        $y += $slope.down
+
+        # check for tree
+        $right = $x % $width
+        $down = $y
+
+        if(($input[$down][$right]) -eq "#") 
+        {
+            $trees++ 
+        }
+
+        #"right: $right  down: $down  ($x,$y)  :: $($input[$down][$right])"
+    } while($y -lt $end)
+
+    $alltrees += $trees 
+    "Slope =  R$($slope.right) D$($slope.down)    Trees =  $trees"
+}
+
+$product = $alltrees[0]
+for($i =1; $i -lt $alltrees.Length; $i++)
+{
+    $product = $product * $alltrees[$i]
+}
+
+"Answer: $($product)"
